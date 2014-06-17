a0a8b8455928e7b9e515e62975940b5c26a2a2c5
Andrew Tran <atran@princeton.edu>
2014-06-17 10:50:49 +0800

    low-pass filtering of correctional angle

--- src/mtrec/rock/compass/MainActivity.java
+++ src/mtrec/rock/compass/MainActivity.java
@@ -49,18 +49,40 @@ public class MainActivity extends Activity {
 	        private int oriNegaNum = 0;
 	        
 	        private float minusAvg = 0; // For gyroscope
+
+	        // custom
+	        private float[] accelVals = new float[3];
+	        private float[] mfVals = new float[3];
+
+	        private float[] matrixR = new float[9];
+	        private float[] matrixI = new float[9];
+	        private float[] matrixVals = new float[3];
 	        
 			@Override
 			public void onSensorChanged(SensorEvent event) {
 				// TODO Auto-generated method stub
 		        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-	            	if (gyroNegaNum > 0 && oriNegaNum > 0){
-	            		float t = 0; // To calculate avg;
-	            		for(int i = 0; i < ARRAY_LENGTH; i++){
-	            			t += 180 * gyroDegreeArray[i] / Math.PI - oriDegreeArray[i];
+	            	if (gyroNegaNum > 0 && oriNegaNum > 0)
+	            	{
+	            		// calculate average
+	            		// float t = 0;
+	            		// for(int i = 0; i < ARRAY_LENGTH; i++){
+	            		// 	t += 180 * gyroDegreeArray[i] / Math.PI - oriDegreeArray[i];
+	            		// }
+	            		// minusAvg = t/ARRAY_LENGTH;
+
+	            		// Log.d("avg", Float.toString(minusAvg));
+
+	            		// low-pass filtering of correctional angle
+	            		float total[] = new float[2];
+	            		for (int i = 0; i < ARRAY_LENGTH; i++)
+	            		{
+	            			double deg = 180 * gyroDegreeArray[i] / Math.PI - oriDegreeArray[i];
+	            			total[0] += Math.cos(deg);
+	            			total[1] += Math.sin(deg);
 	            		}
-	            		minusAvg = t/ARRAY_LENGTH;
-	            		Log.d("avg", Float.toString(minusAvg));
+	            		double length = Math.sqrt((total[0] * total[0]) + (total[1] * total[1]));
+	            		minusAvg = (float)Math.atan2(total[1], total[0]);
 	            	}
 		            MainActivity.this.cam1.setOrientation((float) (180 * gyroDegree / Math.PI) - minusAvg);
 		        }
@@ -68,7 +90,35 @@ public class MainActivity extends Activity {
 		        if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
 			       	MainActivity.this.cam2.setOrientation((float)(180.0D * -Math.atan2(event.values[1], event.values[0]) / Math.PI));
 		        }
-		        
+
+				// switch (event.sensor.getType())
+				// {
+				// 	case Sensor.TYPE_ACCELEROMETER:
+				// 		for (int i = 0; i < 3; i++)
+				// 		{
+				// 			accelVals[i] = event.values[i];
+				// 		}
+				// 		break;
+				// 	case Sensor.TYPE_MAGNETIC_FIELD:
+				// 		for (int i = 0; i < 3; i++)
+				// 		{
+				// 			mfVals[i] = event.values[i];
+				// 		}
+				// 		break;
+				// }
+		  //       boolean success = SensorManager.getRotationMatrix(matrixR, matrixI, accelVals, mfVals);
+		  //       if (success)
+		  //       {
+		  //       	SensorManager.getOrientation(matrixR, matrixVals);
+		  //       	double azimuth = Math.toDegrees(matrixVals[0]);
+		  //       	double pitch = Math.toDegrees(matrixVals[1]);
+		  //       	double roll = Math.toDegrees(matrixVals[2]);
+		  //       	MainActivity.this.cam1.setOrientation((float)azimuth);
+		  //       }
+
+		  //       MainActivity.this.cam2.setOrientation((float)1.0);
+
+
 		        if (event.sensor.getType() == Sensor.TYPE_ORIENTATION) {
 		        	oriDegree = (event.values[0] + 90 < 360)? (event.values[0] + 90.0F) : (event.values[0] + 90 - 360);
 	            	if(oriCounter >= ARRAY_LENGTH){
